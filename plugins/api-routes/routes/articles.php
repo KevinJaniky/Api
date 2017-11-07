dcf16befe335ee40c1964f660615d4f567989c40
KevinJaniky <kevin.janiky@gmail.com>
2017-11-07 23:30:11 +0100

    update plugin api route

--- plugins/api-routes/routes/articles.php
+++ plugins/api-routes/routes/articles.php
@@ -22,6 +22,29 @@ function get_articles_types($data)
     return $items;
 }
 
+function get_articles_types_per_page($data)
+{
+    $posts = get_posts(array(
+        'post_type' => 'post',
+        'numberposts' => $data['nb'],
+    ));
+    if (empty($posts)) return null;
+    $items = array();
+    $count = count($posts);
+    for ($i = 0; $i < $count; $i++) {
+        $id = $posts[$i]->ID;
+        $items[$i]['id'] = $posts[$i]->ID;
+        $items[$i]['uri'] = $posts[$i]->post_name;
+        $items[$i]['date'] = $posts[$i]->post_date;
+        $items[$i]['content'] = $posts[$i]->post_content;
+        $items[$i]['title'] = $posts[$i]->post_title;
+        $taxo = get_the_terms($id, 'category');
+        $items[$i]['taxo'] = $taxo[0]->name;
+        $items[$i]['img'] = get_the_post_thumbnail_url($id, 'full');
+    }
+    return $items;
+}
+
 function get_articles_types_by_id($data)
 {
     $posts = get_post($data['id']);
@@ -53,6 +76,10 @@ function add_routes_articles_to_api()
     register_rest_route('types/content', '/articles/(?P<id>\d+)', array(
         'methods' => 'GET',
         'callback' => 'get_articles_types_by_id',
+    ));
+    register_rest_route('types/content', '/articles/per_page/(?P<nb>\d+)', array(
+        'methods' => 'GET',
+        'callback' => 'get_articles_types_per_page',
     ));
 }
 
--- plugins/api-routes/routes/prestations.php
+++ plugins/api-routes/routes/prestations.php
@@ -3,9 +3,41 @@
 
 function get_prestation_types($data)
 {
-    $posts = get_posts(array(
-        'post_type' => 'prestations',
-    ));
+        $posts = get_posts(array(
+            'post_type' => 'prestations',
+        ));
+
+
+    if (empty($posts)) return null;
+    $items = array();
+    $count = count($posts);
+    for ($i = 0; $i < $count; $i++) {
+        $id = $posts[$i]->ID;
+        $fields = get_field_objects($id);
+        if ($posts[$i]->post_status == 'publish') {
+            if (empty($fields)) return null;
+            $items[$i]['id'] = $posts[$i]->ID;
+            $items[$i]['title'] = $posts[$i]->post_title;
+            $items[$i]['uri'] = $posts[$i]->post_name;
+            $items[$i]['description'] = $posts[$i]->post_content;
+            $items[$i]['price'] = $fields['prestation_price']['value'];
+            $items[$i]['link'] = $fields['prestation_lien']['value'];
+            $taxo = get_the_terms($id, 'prestation-taxo-item');
+            $items[$i]['taxo'] = $taxo[0]->name;
+            $items[$i]['img'] = get_the_post_thumbnail_url($id, 'full');
+        }
+    }
+    return $items;
+}
+
+
+function get_prestation_types_per_page($data)
+{
+        $posts = get_posts(array(
+            'post_type' => 'prestations',
+            'numberposts' => $data['nb'],
+        ));
+
 
     if (empty($posts)) return null;
     $items = array();
@@ -61,6 +93,10 @@ function add_routes_prestation_to_api()
     register_rest_route('types/content', '/prestations/', array(
         'methods' => 'GET',
         'callback' => 'get_prestation_types',
+    ));
+    register_rest_route('types/content', '/prestations/per_page/(?P<nb>\d+)', array(
+        'methods' => 'GET',
+        'callback' => 'get_prestation_types_per_page',
     ));
     register_rest_route('types/content', '/prestations/(?P<id>\d+)', array(
         'methods' => 'GET',
